40171610e66bebd81bf318d49918f5c28e2d296c
Makeen Khawajkeh <makeenkh2004@gmail.com>
2025-09-04 11:45:23 +0300

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,37 @@
+// src/components/Hero.jsx
+import React from 'react';
+
+const Hero = () => {
+return (
+<section id="home" className="bg-[#1F476F] text-white py-20 md:py-32 mt-20 h-[calc(100vh-80px)]">
+    <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4 ">
+    {/* قسم المحتوى (النص) */}
+    <div className="md:w-1/2 text-center md:text-left mb-8 md:mb-0">
+        <h1 className="text-4xl md:text-6xl font-bold leading-tight mb-4">
+        Welcome to <span className="text-[#B88647]">Loujico</span> Company.
+        </h1>
+        <p className="text-lg md:text-xl text-gray-300 mb-6">
+        Loujico is a company specializing in providing consulting, management, training, and research services to companies and clients. The company seeks to support companies' performance and achieve their strategic goals by offering innovative and customized solutions, with a focus on improving efficiency and reducing daily burdens.
+        </p>
+        <a
+        href="#contact"
+        className="inline-block bg-[#B88647] hover:bg-[#a17a41] text-white font-bold py-3 px-6 rounded-full transition duration-300"
+        >
+        Contact Us
+        </a>
+    </div>
+
+    {/* قسم الصورة */}
+    <div className="md:w-1/2 flex justify-center md:justify-end">
+        <img
+        src="/public/assets/image/Hero.png"
+        alt="صورة تعبر عن خدمات الشركة"
+        className=" lg:w-[500px] md:w-[400px] w-[300px] rounded-full object-cover"
+        />
+    </div>
+    </div>
+</section>
+);
+};
+
+export default Hero;
